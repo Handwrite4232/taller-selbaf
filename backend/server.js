0ee4a15b6d588f8a05da815a00765d5667e44e2c
Handwrite4232 <Blustery4@proton.me>
2026-06-06 09:28:27 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const DB_PATH = path.join(__dirname, 'data', 'turnos.json');
 
-<<<<<<< HEAD
+
 app.use(cors());
 app.use(express.json());
 
-// Estructura de Swagger conforme al informe
-=======
 
 app.use(express.json());
 
@@ -25,7 +23,7 @@ let turnos = [
 ];
 
 // Documentación Swagger
->>>>>>> 261c3cb066318dd935c17b97aa63cdf65ad71ac9
+
 const swaggerDocument = {
   openapi: "3.0.0",
   info: {
@@ -52,11 +50,10 @@ const swaggerDocument = {
     }
   }
 };
-<<<<<<< HEAD
-=======
+
 
 // Ruta para la interfaz gráfica 
->>>>>>> 261c3cb066318dd935c17b97aa63cdf65ad71ac9
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // 1. GET con detección de errores
@@ -124,11 +121,10 @@ app.put('/api/turnos/:id', async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
+
 app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
-=======
+
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
   console.log(`Documentación de Swagger disponible en http://localhost:${PORT}/api-docs`);
 });
->>>>>>> 261c3cb066318dd935c17b97aa63cdf65ad71ac9
